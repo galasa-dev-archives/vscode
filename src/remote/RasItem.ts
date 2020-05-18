a2da27c53fd9a89eb7a8a917b093f7a78538ab88
LukasMarivoetIBM <lukas.marivoet@ibm.com>
2020-05-18 16:58:48 +0200

    Opening Remote files refactored
    Signed-off-by: LukasMarivoetIBM <lukas.marivoet@ibm.com>

--- src/remote/RasItem.ts
+++ src/remote/RasItem.ts
@@ -6,6 +6,7 @@ export class RasItem extends vscode.TreeItem{
     constructor(public label: string,
                 public directory: boolean,
                 public children: any[] | undefined,
+                public data:any | undefined,
                 public readonly collapsibleState: vscode.TreeItemCollapsibleState,
                 public contextValue : string) {
         super(label, collapsibleState )
