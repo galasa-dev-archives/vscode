e6ba8bfa95384fcb0acea5d0b5c82b7dfcfcb556
Richard Somers <rsomers1@sheffield.ac.uk>
2020-06-01 15:50:01 +0100

    Tidy up
    Signed-off-by: Richard Somers <rsomers1@sheffield.ac.uk>

--- src/TestExtractor.ts
+++ src/TestExtractor.ts
@@ -1,18 +1,8 @@
-import * as vscode from 'vscode';
-const path = require('path');
+export class TestCase{
 
-export class TestCase extends vscode.TreeItem {
-
-    constructor(
-        public readonly label: string,
-        public readonly pathToFile: string
-	) {
-		super(label);
-    }
-    
-    iconPath = {
-		light: path.join(__filename, '..', '..', 'resources', 'GalasaLogo.svg'),
-		dark: path.join(__filename, '..', '..', 'resources', 'GalasaLogo.svg')
-	};
+  constructor(
+      public readonly label: string,
+      public readonly pathToFile: string
+  ) {}
 
 }
