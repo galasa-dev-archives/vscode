9e562684053b27ca5cc60a4f4403a3ed3ab9b9e8
LukasMarivoetIBM <lukas.marivoet@ibm.com>
2020-06-03 15:47:51 +0200

    terminal
    Signed-off-by: LukasMarivoetIBM <lukas.marivoet@ibm.com>

--- src/ui/TerminalField.ts
+++ src/ui/TerminalField.ts
@@ -5,14 +5,16 @@ export class TerminalField {
     public column:number;
     private fieldProtected:boolean;
     private fieldDisplay:boolean;
+    public unformatted:boolean;
     public contents:FieldContents[];
 
     constructor(row:number, column:number, fieldProtected:boolean, 
-                    fieldDisplay:boolean, contents:FieldContents[]) {
+                    fieldDisplay:boolean, unformatted:boolean, contents:FieldContents[]) {
         this.row = row;
         this.column = column;
         this.fieldProtected = fieldProtected;
         this.fieldDisplay = fieldDisplay;
+        this.unformatted = unformatted;
         this.contents = contents;
     }  
 }
